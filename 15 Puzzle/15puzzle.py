7562103189c9dea690c197a92e0bf546471affc6
Truvam <truvam@users.noreply.github.com>
2018-03-01 18:54:15 +0000

    Added 2 heuristics: Misplaced pieces and Manhattan distance | Fixed Greedy

--- 15 Puzzle/15puzzle.py
+++ 15 Puzzle/15puzzle.py
@@ -2,7 +2,6 @@
 
 
 from prettytable import PrettyTable
-#from memory_profiler import memory_usage #not needed, replaced by resource
 import math
 import time
 import resource
@@ -15,15 +14,14 @@ n_nodes = 0
 
 class Node:
     def __init__(self, state, parent, operator, depth, path_cost):
-        self.state = state  #array from puzzle
+        self.state = state
         self.parent = parent
-        self.children = list() #needs to be a list of nodes
+        self.children = list()
         self.operator = operator
         self.depth= depth
         self.path_cost = path_cost
 
-    
-    #Priority Queue needs to be able to order by path cost
+
     def __lt__(self, other):
         return self.path_cost < other.path_cost
 
@@ -36,21 +34,14 @@ class Node:
 
 
     def move(self):
-        # position can be 1, 2, 3, 4 = up, down, left, right
-
-        new_state = self.state[:]  # [:] is needed or original sate will be
+        new_state = self.state[:]  #[:] is needed or original sate will be
         blank_index = self.state.index(0)
         global n_nodes
-        # changed
-        # print("State:")
-        # print_puzzle(state)
+
         if blank_index not in [0, 1, 2, 3]: # UP
             tmp = new_state[blank_index - 4]
             new_state[blank_index - 4] = new_state[blank_index]
             new_state[blank_index] = tmp
-            #print_puzzle(self.state)
-            #print("Up")
-            #print_puzzle(new_state)
             child = Node(new_state, self, "Up", self.depth + 1, self.path_cost)
             self.children.append(child)
             n_nodes += 1
@@ -60,9 +51,6 @@ class Node:
             tmp = new_state[blank_index + 4]
             new_state[blank_index + 4] = new_state[blank_index]
             new_state[blank_index] = tmp
-            #print_puzzle(state)
-            #print("Down")
-            #print_puzzle(new_state)
             child = Node(new_state, self, "Down", self.depth + 1, self.path_cost)
             self.children.append(child)
             n_nodes += 1
@@ -72,9 +60,6 @@ class Node:
             tmp = new_state[blank_index - 1]
             new_state[blank_index - 1] = new_state[blank_index]
             new_state[blank_index] = tmp
-            #print_puzzle(state)
-            #print("Left")
-            #print_puzzle(new_state)
             child = Node(new_state, self, "Left", self.depth + 1, self.path_cost)
             self.children.append(child)
             n_nodes += 1
@@ -84,9 +69,6 @@ class Node:
             tmp = new_state[blank_index + 1]
             new_state[blank_index + 1] = new_state[blank_index]
             new_state[blank_index] = tmp
-            #print_puzzle(state)
-            #print("Right")
-            #print_puzzle(new_state)
             child = Node(new_state, self, "Right", self.depth + 1, self.path_cost)
             self.children.append(child)
             n_nodes += 1
@@ -98,7 +80,6 @@ def print_puzzle(state):
     table.add_row(state[4:8])
     table.add_row(state[8:12])
     table.add_row(state[12:16])
-
     print(table)
 
 
@@ -135,61 +116,51 @@ def has_solution(config):
     """
     n_inv = 0
     blank_row = math.ceil((16 - config.index(0)) / 4)
-    #print("Blank Row: %d" % blank_row)
     for i in range(0, puzzle_size):
         for j in range(i+1, puzzle_size):
             if config[i] > config[j] and config[j] != 0:
-                #print("%d %d" %(config[i], config[j]))
                 n_inv += 1
-    #print("number of inversions: %d" % n_inv)
     return (blank_row % 2 != 0) == (n_inv % 2 == 0)
     
 
-def DFS(initialConfig, finalConfig): #verificar se o no ja existe
+def DFS(initialConfig, finalConfig):
     stack = list()
     stack.append(Node(initialConfig, None, "", 0, 0))
     visited = list()
-    #path_solution = list()
     GoalFound = False
     max_depth = 15
+
     while stack and not GoalFound:
         node = stack.pop()
         visited.append(node)
         node.move()
-        #print_puzzle(node.state)
-        #try to do moves at random order, to solve the depth problem
         if max_depth > node.depth:
             for child in node.children:
                 if child.is_samePuzzle(finalConfig):
                     print("Goal Found!")
                     GoalFound = True
                     path_solution(child)
-                    #return path_solution
                 if not contains(stack, child) and not contains(visited, child):
                     stack.append(child)
-    #return path_solution
 
 
-def BFS(initialConfig, finalConfig): #verificar se o no ja existe
+def BFS(initialConfig, finalConfig):
     queue = list()
     queue.append(Node(initialConfig, None, "", 0, 0))
     visited = list()
-    #path_solution = list()
     GoalFound = False
+
     while queue and not GoalFound:
         node = queue.pop(0)
         visited.append(node)
         node.move()
-        #print_puzzle(node.state)
         for child in node.children:
             if child.is_samePuzzle(finalConfig):
                 print("Goal Found!")
                 GoalFound = True
                 path_solution(child)
-                #return path_solution
             if not contains(queue, child) and not contains(visited, child):
                 queue.append(child)
-    #return path_solution
 
 
 def IDFS(initialConfig, finalConfig):
@@ -206,11 +177,11 @@ def DLS(initialConfig, finalConfig, depth):
     stack.append(Node(initialConfig, None, "", 0, 0))
     visited = list()
     GoalFound = False
+
     while stack:
         node = stack.pop()
         visited.append(node)
         node.move()
-        # print_puzzle(node.state)
         if depth > node.depth:
             for child in node.children:
                 if child.is_samePuzzle(finalConfig):
@@ -225,24 +196,14 @@ def DLS(initialConfig, finalConfig, depth):
 
 def Greedy(initialConfig, finalConfig, heuristic):
     pq = PriorityQueue()
-    node = Node(initialConfig, None, "", 0, 0)
-    #print_puzzle(node.state)
-    
-    if heuristic == '1':
-        cost = node.depth + heuristic_place(node.state, finalConfig)
-    else:
-        cost = node.depth + heuristic_manhattan(node.state, finalConfig)
-    
-    pq.put(node, cost)
+    pq.put(Node(initialConfig, None, "", 0, 0))
     visited = list()
     GoalFound = False
 
     while pq and not GoalFound:
         node = pq.get()
-        #visited.append(node)
+        visited.append(node)
         node.move()
-        #print_puzzle(node.state)
-        #print("cost: %d" % node.path_cost) 
         for child in node.children:
             if child.is_samePuzzle(finalConfig):
                 print("Goal Found!")
@@ -250,7 +211,7 @@ def Greedy(initialConfig, finalConfig, heuristic):
                 path_solution(child)
             if not contains(pq.queue, child) and not contains(visited, child):
                 if heuristic == '1':
-                    cost = heuristic_place(child.state, finalConfig)
+                    cost = heuristic_misplace(child.state, finalConfig)
                     child.path_cost = cost
                 else:
                     cost = heuristic_manhattan(child.state, finalConfig)
@@ -260,31 +221,20 @@ def Greedy(initialConfig, finalConfig, heuristic):
 
 def A_Star(initialConfig, finalConfig, heuristic):
     pq = PriorityQueue()
-    node = Node(initialConfig, None, "", 0, 0)
-    #print_puzzle(node.state)
-    
-    if heuristic == '1':
-        cost = node.depth + heuristic_place(node.state, finalConfig)
-    else:
-        cost = node.depth + heuristic_manhattan(node.state, finalConfig)
-    
-    pq.put(node, cost)
-    visited = list()
+    pq.put(Node(initialConfig, None, "", 0, 0))
     GoalFound = False
 
     while pq and not GoalFound:
         node = pq.get()
-        #visited.append(node)
         node.move()
-        #print_puzzle(node.state)
-        #print("cost: %d" % node.path_cost) 
         for child in node.children:
             if child.is_samePuzzle(finalConfig):
                 print("Goal Found!")
                 GoalFound = True
                 path_solution(child)
             if heuristic == '1':
-                cost = child.depth + heuristic_place(child.state, finalConfig)
+                cost = child.depth + heuristic_misplace(child.state,
+                                                        finalConfig)
                 child.path_cost = cost
             else:
                 cost = child.depth + heuristic_manhattan(child.state, 
@@ -293,34 +243,26 @@ def A_Star(initialConfig, finalConfig, heuristic):
             pq.put(child, cost)
 
 
-def heuristic_manhattan(state, finalConfig): #verificar se esta correto
-    #print("Manhattan")
+def heuristic_manhattan(state, finalConfig):
     cont = 0
-    #print_puzzle(state)
-    #print_puzzle(finalConfig)
-    #tem que ser o numero de movimentos necessarios para chegar a posição correta
-    for i in range(0, 15):
-        n = finalConfig.index(i) - state.index(i)
-        #print("N: %d" % n)
-        cont += manhattan_aux(n)
 
-    print(cont)
+    for i in range(0, 16):
+        cont += manhattan_aux(state.index(i), finalConfig.index(i))
     return cont
 
 
-def manhattan_aux(n):
-    for j in range(-1, 15):
-        for k in range(-1, 15):
-            #print("J: %d + K: %d" % (j, k))
-            #print("SUM: %d" % (j*4 + k*4))
-            if (j*4 + k*1) == n:
-                print("J: %d + K: %d" % (j, k))
-                #print("Found")
-                return abs(j + k)
-    return 0
+matrix_ij = {0:(1,1), 0.25:(1,2), 0.50:(1,3), 0.75:(1,4),
+             1:(2,1), 1.25:(2,2), 1.50:(2,3), 1.75:(2,4),
+             2:(3,1), 2.25:(3,2), 2.50:(3,3), 2.75:(3,4),
+             3:(4,1), 3.25:(4,2), 3.50:(4,3), 3.75:(4,4)}
+
+def manhattan_aux(i, j):
+    x1, y1 = matrix_ij[i/4]
+    x2, y2 = matrix_ij[j/4]
+    return abs(x1-x2) + abs(y1-y2)
 
 
-def heuristic_place(state, finalConfig):
+def heuristic_misplace(state, finalConfig):
     """
     This heuristics checks the number of out of place pieces.
     :param initialConfig: First state;
@@ -329,10 +271,8 @@ def heuristic_place(state, finalConfig):
     """
     h = 0
     for i in range(0, 16):
-        #print("%d %d" % (state[i], finalConfig[i]))
         if state[i] != finalConfig[i]:
             h += 1
-    #print("h: %d" % h)
     return h
 
 
@@ -346,29 +286,16 @@ def contains(listNode, Node):
 
 def path_solution(Node):
     node = Node
-    #path = list()
-    #path.append(node)
     directions = list()
     directions.append(node.operator)
-    #print_puzzle(node.state)
-    #print(node.direction)
     depth = node.depth
-    path_cost = node.path_cost
     while node.parent is not None:
         node = node.parent
-        #print_puzzle(node.state)
-        #print(node.direction)
-        #path.append(node.parent)
         directions.append(node.operator)
     directions.pop()
     print("Path to solution: ", end="")
     print(directions)
     print("Depth: %d" % depth)
-    print("Path Cost: %d" % path_cost)
-    #print("Time to finish: ")
-    #print("Memory used: ")
-    #print("Depth/Cost: ")
-    #return path
 
 
 def execute(option, initialConfig, finalConfig, heuristic):
@@ -377,20 +304,25 @@ def execute(option, initialConfig, finalConfig, heuristic):
     global n_nodes
 
     if option == '1':
-        print("DFS")
+        print("Using: DFS")
         DFS(initialConfig, finalConfig)
     elif option == '2':
         print("Using: BFS")
         BFS(initialConfig, finalConfig)
-        # quantos movimentos sao necessarios para encontrar solução
     elif option == '3':
-        print("IDFS")
+        print("Using: IDFS")
         IDFS(initialConfig, finalConfig)
     elif option == '4':
-        print("Greedy")
+        if heuristic == '1':
+            print("Using: Greedy + Misplaced Pieces")
+        else:
+            print("Using: Greedy + Manhattan Distance")
         Greedy(initialConfig, finalConfig, heuristic)
     elif option == '5':
-        print("A*")
+        if heuristic == '1':
+            print("Using: A* + Misplaced Pieces")
+        else:
+            print("Using: A* + Manhattan Distance")
         A_Star(initialConfig, finalConfig, heuristic)
 
     end = time.time()
@@ -403,16 +335,16 @@ def execute(option, initialConfig, finalConfig, heuristic):
 def main():
     initialConfig = list(map(int, input("Initial Configuration: ").split()))
     finalConfig = list(map(int, input("Final Configuration: ").split()))
-    #print(initialConfig)
-    #print(finalConfig)
     if not (has_solution(initialConfig) == has_solution(finalConfig)):
         print("This 15 puzzle has no solution.")
     else:
         print("This 15 puzzle has solution.")
         create_menu(1)
         option = input('Option: ')
-        create_menu(2)
-        heuristic = input('Option: ')
+        heuristic = 0
+        if option not in ['1', '2', '3']:
+            create_menu(2)
+            heuristic = input('Option: ')
         execute(option, initialConfig, finalConfig, heuristic)
 
 
