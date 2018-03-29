3ad8c2b62beac8da3c1853da6fa4c11006ab369f
Truvam <truvam@users.noreply.github.com>
2018-03-29 12:10:24 +0100

    Added get_route

--- TripPlan/trip_plan.py
+++ TripPlan/trip_plan.py
@@ -58,28 +58,35 @@ class Database:
             print(str(e))
             return False
 
-    def print_database(self, place1=None, place2=None, days=0):
+    def print_database(self, place1=None, place2=None, days=0,
+                       destination=False):
         days_list = list()
         found = False
+        table = PrettyTable(["Starting", "Destination"])
         for route in self.Route:
             if route.Place1 == place1 and route.Place2 == place2 or place1 is\
                     None and place2 is None:
-                print("Starting : " + route.Place1)
-                print("Destination: " + route.Place2)
-                table = PrettyTable(["Departure Time", "Arrival Time", "Flight "
-                                                                       "Number",
-                                     "Days"])
-                for i in range(len(route.Flights)):
-                    table.add_row([route.Flights[i].departureTime,
-                                   route.Flights[
-                        i].arrivalTime, route.Flights[i].flightNumber,
-                                   route.Flights[i].listDays])
-                    days_list.append(route.Flights[i].listDays)
-                    found = True
-                print(table)
-                print()
+                if not destination:
+                    print("\nStarting : " + route.Place1)
+                    print("Destination: " + route.Place2)
+                    table = PrettyTable(["Departure Time", "Arrival Time", "Flight "
+                                                                           "Number",
+                                         "Days"])
+                    for i in range(len(route.Flights)):
+                        table.add_row([route.Flights[i].departureTime,
+                                       route.Flights[
+                            i].arrivalTime, route.Flights[i].flightNumber,
+                                       route.Flights[i].listDays])
+                        days_list.append(route.Flights[i].listDays)
+                        found = True
+                    print(table)
+                    print()
+                else:
+                    table.add_row([route.Place1, route.Place2])
         if days == 1:
             return days_list
+        elif destination:
+            print(table)
         else:
             return found
 
@@ -107,6 +114,39 @@ def direct_flights(place1, place2, db):
                     i += 1
 
 
+def get_route(place1, place2, db, r=list()):
+    r.append(place1)
+    place3 = ""
+    found = False
+    if not db.print_database(place1, place2):
+        while True:
+            for route in db.Route:
+                if route.Place1 == place1 and route.Place2 == place2:
+                    r.append(place2)
+                    found = True
+                    break
+                elif route.Place1 == place3 and route.Place2 == place2:
+                    r.append(place2)
+                    found = True
+                    break
+                elif route.Place1 == place1:
+                    place3 = route.Place2
+                    r.append(place3)
+                    break
+            if found:
+                break
+        if found:
+            print("\n\nROUTE: Starting: " + r[0], end="")
+            for i in range(1, len(r)):
+                print(" ---> " + r[i], end="")
+            print(" :Destination")
+            print("\nAvailable flights:")
+            for i in range(0, len(r)-1):
+                db.print_database(r[i], r[i+1])
+        else:
+            print("There are no available flights.")
+
+
 def entire_days(days):
     days_list = list()
     entire_days = {"su": "Sunday", "mo": "Monday", "tu": "Tuesday",
@@ -127,7 +167,8 @@ def main():
     db = Database()
     if db.connect():
         print("Database connected!")
-        # db.print_database()
+        print("  Available destinations:")
+        db.print_database(destination=True)
     else:
         exit()
 
@@ -143,6 +184,7 @@ def main():
     elif option == 2:
         place1 = input("Place1: ")
         place2 = input("Place2: ")
+        get_route(place1, place2, db)
     else:
         cities = list()
         initial_city = input("Initial City: ")
