12a82db83920d91556b8332d1bca063b3f1a5824
Truvam <truvam@users.noreply.github.com>
2018-05-06 11:49:40 +0100

    Rearrange code | Added print_tree

--- DecisionTrees/main.py
+++ DecisionTrees/main.py
@@ -5,18 +5,21 @@ import csv
 import glob
 import sys
 from prettytable import PrettyTable
+from decision_tree import id3
+frequency = {}
 
 
 class Data:
     def __init__(self, name, header, rows):
         self.name = name
-        self.header = header
-        self.rows = rows
+        self.attributes = header
+        self.values = rows
+        self.length = len(rows)
 
     def print_data(self):
         print(self.name)
-        table = PrettyTable(self.header)
-        for row in self.rows:
+        table = PrettyTable(self.attributes)
+        for row in self.values:
             table.add_row(row)
         print(table)
 
@@ -28,52 +31,52 @@ def find_files(argv=None):
             print("No csv files found.")
             exit()
         else:
-            print("Files found: ", end='')
-            print(argv)
-            op = input("Do you want to load them? [y/n]: ")
-            if op in "yY" or op in "yesYes":
-                data = load_csv(argv)
-                return data
-            else:
-                print("Exiting...")
-                exit()
+            d = {i: argv[i] for i in range(len(argv))}
+            print("Files found: ", d)
+            n = int(input("Choose number of file: "))
+            data = load_csv(argv[n])
+            return data
     else:
-        data = load_csv(argv)
+        data = load_csv(argv[0])
         return data
 
 
-def load_csv(argv):
-    print("Loading csv files: ", end='')
-    print(argv)
-    data = list()
-    for file in argv:
-        with open(file, 'r') as csv_file:
-            reader = csv.reader(csv_file)
-            content = list(reader)
-        data.append(Data(file, content[0], content[1:]))
+def load_csv(file):
+    print("Loading csv file: " + file)
+    with open(file, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        content = list(reader)
+    data = Data(file, content[0], content[1:])
     return data
 
 
-def get_data(data):
-    for i in range(len(data)):
-        data[i].print_data()
+def count_value(values, attributes, key):
+    attr_index = attributes.index(key)
+    for val in values:
+        if (key, val[attr_index], val[-1]) in frequency:
+            frequency[(key, val[attr_index], val[-1])] += 1
+        else:
+            frequency[(key, val[attr_index], val[-1])] = 1
 
 
-def id3(examples, attributes, parent_examples):
-    if examples is None:
-        return plurality_value(parent_examples)
-    elif same_classification(examples):
-        return classification
-    elif attributes is None:
-        return plurality_value(examples)
+def print_tree(values, attributes, tree, level=0, before=""):
+    if not tree or len(tree) == 0:
+        print("\t" * level, "-")
     else:
-        # A = argmax(a in attributes)importance(a, examples)
-        # tree = new_decision_tree(test A)
-        for value in vk(A):
-            # exs = {e : e in examples and e.A = vk}
-            subtree = id3(exs, attributes - A, examples)
-            # tree.add_branch(label=(A = vk), subtree=subtree)
-        return tree
+        key_before = ""
+        for key, value in tree.items():
+            if isinstance(value, dict):
+                if key in attributes:
+                    print("\t" * level, "<" + key + ">:")
+                    count_value(values, attributes, key)
+                    key_before = key
+                else:
+                    print("\t" * level, key + ":")
+                # print(frequency)
+                print_tree(values, attributes, value, level+1, key_before)
+            else:
+                print("\t" * level, key + ":", value,
+                      "(" + str(frequency[(before, key, value)]) + ")")
 
 
 def main():
@@ -81,7 +84,12 @@ def main():
         data = find_files(argv=sys.argv[1:])
     else:
         data = find_files()
-    get_data(data)
+    data.print_data()
+
+    target_attr = data.attributes[-1]
+    tree = id3(data.values, data.attributes, target_attr)
+
+    print_tree(data.values, data.attributes, tree)
 
 
 if __name__ == "__main__":
